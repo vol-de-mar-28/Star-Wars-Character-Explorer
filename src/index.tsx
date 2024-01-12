c8609a20398672799e163e38525b360f4090c051
Volodymyr Marukhno <vlama4me@gmail.com>
2024-01-12 14:14:33 +0200

    delete trash

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { store, persistor } from './redux/store';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -20,8 +18,3 @@ root.render(
       </PersistGate>
   </ReduxProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
